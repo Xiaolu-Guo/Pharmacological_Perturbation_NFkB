1b903cb0f5144fc988391d1ecd1a1c0fa1560c02
erbozich <ebozich@g.ucla.edu>
2025-02-01 17:47:29 -0800

    Added error plot

--- visualize_fPCA_temp_patterns_figS2.m
+++ visualize_fPCA_temp_patterns_figS2.m
@@ -1,29 +1,48 @@
+fig_path = '../FigS2/';
 %% Load 5 component fPCA results
-filename = '../fpca_5c_components_090623.pkl';
+filename = 'fpca_5c_components_090623.pkl';
 fid = py.open(filename,'rb');
 fpca_load = py.pickle.load(fid);
 fpca_components = double(fpca_load.data_matrix);
 [num_comps, timepoints] = size(fpca_components);
 
 
-%% Temporal pattern plots
+%% Component plots
 x = 1:1:timepoints;
-
-traj_colors = {[0 0.4470 0.7410], [0.8500 0.3250 0.0980], [0.9290 0.6940 0.1250], ...	
-              [0.4940 0.1840 0.5560],[0.4660 0.6740 0.1880],... 
-              [0.3010 0.7450 0.9330], [0.6350 0.0780 0.1840]};
 figure()
 for i = 1:num_comps
-    subplot(4, 4, i + 1)
-    plot(x, fpca_components(i,:), 'LineWidth', 2, 'color', traj_colors{i});
-    %set(gca, 'xtick', [0:240:480], 'xticklabels', [], ...
-    %            'ytick', [-4:2:4], 'yticklabels', [])
+    plot(x, fpca_components(i,:), 'LineWidth', 2.5);
+    set(gca, 'xtick', [0:240:480], 'xticklabels', [], ...
+                'ytick', [-4:2:4], 'yticklabels', [])
     ylim([-4, 4])
     xlim([0, 481])
-    set(gca, 'xtick', [], 'ytick', [])
-    ax=gca;
-    ax.LineWidth = 2;
+    hold on 
 end
+ax=gca;
+ax.LineWidth = 1.5;
 
-fig_path = '../FigS2/';
+for i = 1:num_comps
+    comp_legend{i} = strcat('Comp.', num2str(i));
+end
+%[lgnd,icons,plots,txt] = legend(comp_legend);
+%temp = [lgnd; lgnd.ItemText];
+%set(temp,'FontSize',10);
+%set(temp,'FontName','Arial');
+
 saveas(gcf,strcat(fig_path,'fPCA_5comp_temp_patterns'),'png')
+%% Variance explained/error plot
+filename = 'fpca_var_explained_092023.pkl';
+fid = py.open(filename,'rb');
+fpca_load = py.pickle.load(fid);
+fpca_ve = double(fpca_load.data);
+
+figure()
+x = 1:1:length(fpca_ve(1:10));
+plot(x, fpca_ve(1:10), '-o', 'MarkerFaceColor', 'k', 'LineWidth', 2, 'color', 'k')
+ylim([0, 1.2])
+set(gca, 'xtick', [1:length(fpca_ve(1:10))], 'xticklabels', [], ...
+    'ytick', [0:0.2:1.2], 'yticklabels', [])
+ax=gca;
+ax.LineWidth = 1.5;
+
+saveas(gcf,strcat(fig_path,'fPCA_error_plot'),'png')
